62e4e84cd63559c7f046ba5512804f31f963a196
Selim Abik <erenabik122@gmail.com>
2024-10-24 17:24:57 -0400

    Rearranged home page to be starting page. Added nav bar. Added Latest projects tab. Added styling to home page

--- nextjs-dashboard/app/page.tsx
+++ nextjs-dashboard/app/page.tsx
@@ -1,26 +1,98 @@
-import AcmeLogo from '@/app/ui/acme-logo';
 import Link from 'next/link';
-import './styles/global.css';
+import styles from './home/page.module.css'; // Using CSS modules
+import carpic from './carpic.jpg'
+import Image from 'next/image';
+
 
 export default function Page() {
   return (
-    <main>
-            
+    <main className={styles.main}>
+      <style>
+      @import url('https://fonts.googleapis.com/css2?family=Alumni+Sans+Pinstripe:ital@0;1&display=swap');
+      </style>
 
-          <div className='sidenav'>
-          <Link href="/home">
+      <div className={styles.sidenav}>
+        <Link href="/home">
           <span>Home</span>
-          </Link>
-          <Link href="/login">
+        </Link>
+        <Link href="/login">
           <span>Contact me</span>
-          </Link>
-          <Link href="/login">
+        </Link>
+        <Link href="/projects">
           <span>Projects</span>
-          </Link>
-          <Link href="/login">
+        </Link>
+        <Link href="/work-experience">
           <span>Work Experience</span>
-          </Link>
-          </div>
+        </Link>
+      </div>
+
+      <div className={styles.content}>
+        <h1 className={styles.homeline}>
+          <span>H</span><span>i</span>
+        </h1>
+
+        <h1 className={styles.introduction}>
+          <span>I</span><span>m</span><span>&nbsp;</span> {}
+          <span className={styles.bigS}>S</span>
+          <span>e</span>
+          <span>l</span>
+          <span>i</span>
+          <span>m</span>
+          <span>,</span>
+        </h1>
+        <h1 className={styles.introduction}>
+          <span>
+            S
+          </span>
+          <span>
+            o
+          </span>
+          <span>
+            f
+          </span>
+          <span>
+            t
+          </span>
+          <span>
+            w
+          </span>
+          <span>
+            a
+          </span>
+          <span>
+            r
+          </span>
+          <span>
+            e 
+          </span>
+          <span>
+          &nbsp;
+          </span>
+          <span>D</span><span>e</span><span>v</span><span>e</span><span>l</span><span>o</span><span>p</span><span>e</span><span>r</span>
+
+        </h1>
+        <h1 className={styles.college}>
+        <span>C</span><span>o</span><span>m</span><span>p</span><span>E</span>
+          <span>
+          &nbsp;
+          </span>
+          <span>a</span><span>t</span>
+          <span>
+          &nbsp;
+          </span>
+          <span className = {styles.school}>M</span><span className = {styles.school}>S</span><span className = {styles.school}>U</span>
+          <span>
+          &nbsp;
+          </span>
+          <span>2</span><span>0</span><span>2</span><span>8</span>
+        </h1>
+        <h1 className = {styles.latestproject}>
+          Latest Project
+
+        </h1>
+        <Image src={carpic} alt="A picture of a car" className={styles.Car} />
+        </div>
     </main>
   );
 }
+/*ADD THE YOUTUE VIDEO IFRAME*/
