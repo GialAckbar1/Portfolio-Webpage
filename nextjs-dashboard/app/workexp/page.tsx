004e56410fbedf6a78f86b2ea33e7bd83b5f476e
Selim Abik <erenabik122@gmail.com>
2024-11-14 16:30:53 -0500

    added to work experiences page

--- nextjs-dashboard/app/workexp/page.tsx
+++ nextjs-dashboard/app/workexp/page.tsx
@@ -1,48 +1,37 @@
 import Link from 'next/link';
 import styles from './page.module.css'; // Using CSS modules
 
+export default function Page() {
+    return (
+        <main>
+            <title>Work Experiences</title>
+            
+            <div className={styles.titleContainer}>
+                <h1 className={styles.title}>
+                    <span>W</span>
+                    <span>o</span>
+                    <span>r</span>
+                    <span>k&nbsp;</span>
+                    <span>E</span>
+                    <span>x</span>
+                    <span>p</span>
+                    <span>e</span>
+                    <span>r</span>
+                    <span>i</span>
+                    <span>e</span>
+                    <span>n</span>
+                    <span>c</span>
+                    <span>e</span>
+                    <span>s</span>
+                </h1>
+            </div>
 
-
-
-export default function Page(){
-
-    return(
-    <main>
-        <title>
-            Work Experiences
-        </title>
-        <h1 className ={styles.title}>
-        Work Experiences
-      </h1>
-
-        <div className={styles.sidenav}>
-        <Link href="/">
-          <span>Home</span>
-        </Link>
-        <Link href="/contact">
-          <span>Contact me</span>
-        </Link>
-        <Link href="/projects">
-          <span>Projects</span>
-        </Link>
-        <Link href="/workexp">
-          <span>Work Experience</span>
-        </Link>
-      </div>
-      
-
-
-
-    </main>
-    
-
-
-
-
-
-
-
-);
-
-
+            <div className={styles.sidenav}>
+                <Link href="/"><span>Home</span></Link>
+                <Link href="/contact"><span>Contact me</span></Link>
+                <Link href="/projects"><span>Projects</span></Link>
+                <Link href="/workexp"><span>Work Experience</span></Link>
+            </div>
+        </main>
+    );
 }
